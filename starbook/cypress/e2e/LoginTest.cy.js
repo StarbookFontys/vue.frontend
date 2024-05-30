c30f977c19e1697c1643854f01b7dcbc89d1d099
Coen <458416@student.fontys.nl>
2024-05-30 13:27:25 +0200

    Changed .cy.js name

--- starbook/cypress/e2e/LoginTest.cy.js
+++ starbook/cypress/e2e/LoginTest.cy.js
@@ -1,12 +1,13 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
+describe('Testing the login functionality with JWT token implementation', () => {
+  it('Visits the app and logs in.', () => {
     cy.visit('/')
     //cy.contains('h1', 'You did it!')
     cy.get('#AccountManagement').click()
     cy.get('#EmailText').type('email2')
     cy.get('#PasswordText').type('password')
+    cy.get('#ToSButton').click()
     cy.get('#LoginButton').click()
     cy.get('#LoginLabel').contains('email2')
   })
